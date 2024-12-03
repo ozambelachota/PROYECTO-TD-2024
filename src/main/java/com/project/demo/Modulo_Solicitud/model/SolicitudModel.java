8ae4531c81c22d2fd40a1d56d81b8182601f6ad0
maldini17 <jim.maldonado.17@gmail.com>
2024-12-03 00:00:50 -0500

    Create SolicitudModel.java

---
+++ src/main/java/com/project/demo/Modulo_Solicitud/model/SolicitudModel.java
@@ -0,0 +1,30 @@
+package main.java.com.project.demo.Modulo_Solicitud.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "solicitud")
+public class SolicitudModel {
+
+    @Id
+
+    @Column(name = "Idsolicitud")
+    public Integer Idsolicitud;
+
+    @Column(name = "fecha")
+    public Date fecha;
+
+    @Column(name = "comentario")
+    public String comentario;
+
+
+
+    
+}
