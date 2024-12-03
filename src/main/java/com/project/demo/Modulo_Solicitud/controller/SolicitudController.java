5bbafe9eeb4a0553753cc784c09f02ba7a1fc387
maldini17 <jim.maldonado.17@gmail.com>
2024-12-03 12:51:31 -0500

    Update SolicitudController.java

--- src/main/java/com/project/demo/Modulo_Solicitud/controller/SolicitudController.java
+++ src/main/java/com/project/demo/Modulo_Solicitud/controller/SolicitudController.java
@@ -1,5 +1,20 @@
 package main.java.com.project.demo.Modulo_Solicitud.controller;
 
+import java.util.List;
+
+import main.java.com.project.demo.Modulo_Solicitud.model.SolicitudModel;
+import main.java.com.project.demo.Modulo_Solicitud.services.SolicitudService;
+
+@RestController
+@RequestMapping("")
 public class SolicitudController {
+
+    @Autowired
+    SolicitudService solicitudService;
+
+    @GetMapping("/getAll")
+    public List<SolicitudModel> getAll(){
+        return solicitudService.findAll();
+    }
     
 }
