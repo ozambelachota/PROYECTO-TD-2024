5e8e64dc90a488ae08d561d89eeecfe5352e52ee
Manuel <manuelenriquebarbaran@gmail.com>
2025-01-02 23:17:17 -0500

    creacion de carpetas

--- reunion/src/main/java/com/reunionconsejo/ReunionApplication.java
+++ reunion/src/main/java/com/reunionconsejo/ReunionApplication.java
@@ -1,4 +1,4 @@
-package com.reunionconsejo.reunion;
+package com.reunionconsejo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
