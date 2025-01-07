1db1575098d4e03fa85d39292f5ab63d1cae6555
Manuel <manuelenriquebarbaran@gmail.com>
2025-01-07 16:02:36 -0500

    creacion de Repository

--- reunion/src/main/java/com/reunionconsejo/applications/reunionService.java
+++
@@ -1,6 +0,0 @@
-package com.reunionconsejo.applications;
-
-public class reunionService {
-    //consulta de bases de datos
-    
-}
---
+++ reunion/src/main/java/com/reunionconsejo/infrastructure/repository/IReunionMiembroRepository.java
@@ -0,0 +1,5 @@
+package com.reunionconsejo.infrastructure.repository;
+
+public class IReunionMiembroRepository {
+    
+}
