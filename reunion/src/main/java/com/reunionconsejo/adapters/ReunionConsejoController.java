7a2f3bdf42deb0957ea56125d711308dce0d7ff5
Manuel <manuelenriquebarbaran@gmail.com>
2025-01-09 22:17:03 -0500

    pequeños cambios

--- reunion/src/main/java/com/reunionconsejo/adapters/ReunionConsejoController.java
+++ reunion/src/main/java/com/reunionconsejo/adapters/ReunionConsejoController.java
@@ -15,9 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
-@Controller()
+@Controller
 @RequestMapping(name = ReunionConsejoEndpoint.reunionConsejoBasic)
 public class ReunionConsejoController {
 
@@ -25,16 +26,15 @@ public class ReunionConsejoController {
     ReunionConsejoService serviceConsejo;
 
     @GetMapping(name = ReunionConsejoEndpoint.reunionConsejoGetAll)
-    
     public List<ReunionConsejoModel>getAll(){
         return serviceConsejo.getAll();
     }
 
-    @GetMapping(name =  ReunionConsejoEndpoint.reunionConsejoCreate)
+    @PostMapping(name =  ReunionConsejoEndpoint.reunionConsejoCreate)
     public ReunionConsejoModel create(@RequestBody ReunionConsejoModel model){
         return serviceConsejo.add(model);
     }
- @GetMapping(name =  ReunionConsejoEndpoint.reunionConsejoBuscarID)
+    @GetMapping(name =  ReunionConsejoEndpoint.reunionConsejoBuscarID)
     public ReunionConsejoModel getById(@PathVariable("id_reunion") int id){
         return serviceConsejo.getById(id);
     }
@@ -45,7 +45,7 @@ public class ReunionConsejoController {
     }
 
     @DeleteMapping(name =  ReunionConsejoEndpoint.reunionConsejoDelete)
-    public boolean delete(@PathVariable("id_reunion") int id){
+    public boolean delete(@PathVariable("id") int id){
         return serviceConsejo.delete(id);
     }
     
