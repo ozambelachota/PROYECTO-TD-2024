a112f0ffb690943bae62dec7b6a717407ede8c30
Manuel <manuelenriquebarbaran@gmail.com>
2025-01-09 23:25:40 -0500

    cambios en reunion miembros

---
+++ reunion/src/main/java/com/reunionconsejo/domain/endpoints/ReunionConsejoEndpoint.java
@@ -0,0 +1,11 @@
+package com.reunionconsejo.domain.endpoints;
+
+
+public class ReunionConsejoEndpoint {
+    public static final String reunionConsejoBasic = "/reunionconsejo";
+    public static final String reunionConsejoGetAll = "/list";
+    public static final String reunionConsejoCreate = "/create";
+    public static final String reunionConsejoDelete = "/delete";
+    public static final String reunionConsejoUpdate = "/update";
+    
+}
