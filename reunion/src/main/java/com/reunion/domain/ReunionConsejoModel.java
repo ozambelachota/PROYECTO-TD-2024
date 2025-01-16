ef8c72c9bfa1d1868baa14c4a2803c69cac76909
Manuel <109305586+ManuelChiriBarbaran@users.noreply.github.com>
2025-01-15 20:28:54 -0500

    Update ReunionConsejoModel.java

--- reunion/src/main/java/com/reunion/domain/ReunionConsejoModel.java
+++ reunion/src/main/java/com/reunion/domain/ReunionConsejoModel.java
@@ -13,22 +13,13 @@ import jakarta.persistence.Table;
 @Table(name = "reunion_consejo")
 public class ReunionConsejoModel {
     @Id
-<<<<<<< Updated upstream:reunion/src/main/java/com/reunionconsejo/domain/ReunionConsejoModel.java
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_reunion")
     private Long id_reunion;
 
     @Column(name = "fecha")
     private Date fecha;
-=======
-    
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name = "id_reunion")
-    private Long id_reunion;
-    @Column(name = "fecha")
-    private Date fecha;
-
->>>>>>> Stashed changes:reunion/src/main/java/com/reunion/domain/ReunionConsejoModel.java
 
     public Long getId_reunion() {
         return id_reunion;
