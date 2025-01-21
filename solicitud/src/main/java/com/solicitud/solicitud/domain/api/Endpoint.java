505c840c585882e6051c3d8ad1f481fd867516d4
maldini17 <jim.maldonado.17@gmail.com>
2025-01-21 12:32:09 -0500

    feature: Enpoint

---
+++ solicitud/src/main/java/com/solicitud/solicitud/domain/api/Endpoint.java
@@ -0,0 +1,10 @@
+package com.solicitud.solicitud.domain.api;
+
+public class Endpoint {
+    public static final String BASE_URL_SOLICITUD = "/api/solicitudes";
+    public static final String GET_SOLICITUDES = "/";
+    public static final String CREATE_SOLICITUD = "/create";
+    public static final String GET_SOLICITUD_BY_ID = "/{id}";
+    public static final String UPDATE_SOLICITUD = "/update";
+    public static final String DELETE_SOLICITUD = "/delete/{id}";
+}
